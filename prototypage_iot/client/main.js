bf8f3966b4fcbdeaf9fc66e0d055fbe142eb6cc6
bentarr <benetierpaul@gmail.com>
2021-06-02 12:54:48 +0200

    Setup IoT APP

--- prototypage_iot/client/main.js
+++ prototypage_iot/client/main.js
@@ -1,270 +1,105 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Mongo } from 'meteor/mongo';
 import { HTTP } from 'meteor/http';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-var preferedLanguageIso = new ReactiveVar();
-var preferedLanguageName = new ReactiveVar();
-var languages = new ReactiveVar();
-var nbPages = new ReactiveVar();
-var movies = new ReactiveVar();
-var nbFilms = new ReactiveVar();
-var genres = new ReactiveVar();
-var page = new ReactiveVar(1);
-var date = new ReactiveVar();
-var searchInput = new ReactiveVar();
-var filtreActif = new ReactiveVar();
-var idGenreActif = new ReactiveVar(0);
-var filtreDate = new ReactiveVar(false);
-var filtrePopulariteDecroissant = new ReactiveVar(false);
-var filtrePopulariteCroissant = new ReactiveVar(false);
-var filtreGenre = new ReactiveVar(false);
-var filtreSearch = new ReactiveVar(false);
+var temp_collection = new Mongo.Collection('temp');
+var hum_collection = new Mongo.Collection('hum');
 
-// [Template] Films
+var highTempHasPublished = new ReactiveVar(false);
+var lowTempHasPublished = new ReactiveVar(false);
+var stateLed = new ReactiveVar(false);
 
-
-
-Template.films.onCreated(function filmsOnCreated() {
-  recupererTousLesGenres();
-  recupererLangagePrefere();
-});
-
-Template.films.helpers({
-  movies() { return movies.get(); },
-  doitSafficher() { return nbFilms.get() > 0; },
-  preferedLanguageIso() { return preferedLanguageIso.get(); },
-  preferedLanguageName() { return preferedLanguageName.get(); }
-});
-
-Template.films.events({
-  //
-});
-
-// [Template] Pagination
-
-Template.pagination.helpers({
-  page() { return page.get(); },
-  doitSafficher() { return nbPages.get() > 1; }
-});
-
-Template.pagination.events({
-  'click #pagePrecedente'() {
-    if (page.get() > 1) {
-      page.set(page.get() - 1);
-      if (filtreSearch.get()) {
-        recupererTousLesFilmsRecherches();
+Template.temp.helpers({
+  // Fonction permettant d'accéder aux données de température côté HTML
+  temp() {
+    // Quand la fonctionnalité de stockage en masse des données
+    // sera opérationnelle, récupération des 10 dernières données
+    // pour remplir un graphe
+    // var temp = temp_collection.find().limit(10);
+    var temp = temp_collection.find();
+    temp.forEach((t) => {
+      // Traitement de la donnée de température et envoi d'un appel
+      // HTTP sur le bon endpoint pour envoyer un message au broker, 
+      // sur le canal de la LED, pour lui demander de s'allumer ou
+      // de s'éteindre
+      if (parseFloat(t.celsius.value) >= 30.00) {
+        if (!highTempHasPublished.get()) {
+          HTTP.call(
+            'GET',
+            'http://localhost:3000/high_temp',
+            {},
+            () => {
+              highTempHasPublished.set(true);
+              lowTempHasPublished.set(false);
+              stateLed.set(true);
+            }
+          );
+        }
       } else {
-        recupererTousLesFilms();
+        if (!lowTempHasPublished.get()) {
+          HTTP.call(
+            'GET',
+            'http://localhost:3000/low_temp',
+            {},
+            () => {
+              highTempHasPublished.set(false);
+              lowTempHasPublished.set(true);
+              stateLed.set(false);
+            }
+          );
+        }
       }
-    }
-  },
-  'click #pageSuivante'() {
-    if (page.get() < nbPages.get()) {
-      page.set(page.get() + 1);
-      if (filtreSearch.get()) {
-        recupererTousLesFilmsRecherches();
-      } else {
-        recupererTousLesFilms();
-      }
-    }
+    })
+    return temp;
   }
 });
 
-// [Template] Filtres
+// Souscription à la fonction asyncrhone de meteor
+// permettant de recevoir de la donnée de manière asynchrone
+Template.temp.onCreated(function () {
+  this.subscribe('publish_temp');
+});
 
-Template.filterselect.onCreated(function filtersOnCreated() {
-  recupererTousLesLangages();
-})
+// Fonction permettant d'accéder aux données d'humidité côté HTML
+Template.hum.helpers({
+  hum() {
+    var hum = hum_collection.find();
+    return hum;
+  }
+});
 
-Template.filterselect.helpers({
-  genres() { return genres.get(); },
-  dateDefaultValue() { return date.get(); },
-  inputDefaultValue() { return searchInput.get(); },
-  languages() { return languages.get(); },
-  preferedLanguageIso() { return preferedLanguageIso.get(); },
-  preferedLanguageName() { return preferedLanguageName.get(); }
-})
+Template.hum.onCreated(function () {
+  this.subscribe('publish_hum');
+});
 
-Template.filterselect.events({
-  'change #filter'(event) {
-    let id = event.target.value;
-    page.set(1);
-    filtrePopulariteCroissant.set(false);
-    filtrePopulariteDecroissant.set(false);
-    searchInput.set('');
-    filtreSearch.set(false);
-    if (id == 3) {
-      // Popularité : par ordre croissant
-      filtrePopulariteCroissant.set(true);
-      filtreActif.set('poc');
-    } else if (id == 4) {
-      // Popularité : par ordre décroissant
-      filtrePopulariteDecroissant.set(true);
-      filtreActif.set('pod');
-    }
-    recupererTousLesFilms();
-  },
-  'change #genres'(event) {
-    idGenreActif.set(event.target.value);
-    page.set(1);
-    searchInput.set('');
-    filtreSearch.set(false);
-    if (idGenreActif.get() == '') {
-      filtreGenre.set(false);
+Template.led.events({
+  'click #addButton'() {
+    console.log("toto");
+    if (!stateLed.get()) {
+      HTTP.call(
+        'GET',
+        'http://localhost:3000/high_temp',
+        {},
+        () => {
+          highTempHasPublished.set(true);
+          lowTempHasPublished.set(false);
+          stateLed.set(true);
+        }
+      );   
     } else {
-      filtreGenre.set(true);
-    }
-    recupererTousLesFilms();
-  },
-  'input #date'(event){
-    date.set(event.target.value);
-    page.set(1);
-    searchInput.set('');
-    filtreSearch.set(false);
-    if (date.get() == null) {
-      filtreDate.set(false);
-    }
-    if (date.get() != null){
-      filtreDate.set(true);
-    }
-    recupererTousLesFilms();
-  },
-  'input #search'(event) {
-    searchInput.set(event.target.value);
-    page.set(1);
-    date.set('');
-    filtrePopulariteCroissant.set(false);
-    filtrePopulariteDecroissant.set(false);
-    filtreActif.set('');
-    filtreGenre.set(false);
-    idGenreActif.set(0);
-    filtreSearch.set(false);
-    if (searchInput.get() != '') {
-      filtreSearch.set(true);
-      recupererTousLesFilmsRecherches();
-    } else {
-      recupererTousLesFilms();
-    }
-  },
-  'change #languages'(event) {
-    preferedLanguageIso.set(event.target.value);
-    preferedLanguageName.set($('#languages option:selected').text());
-    updatePreferedLanguage(preferedLanguageIso.get(), preferedLanguageName.get());
-    recupererTousLesFilms();
-  }
-})
-
-// Fonctions
-
-function recupererTousLesFilms() {
-  let pageActuelle = page.get();
-  let filtre = '';
-  let genre = '';
-  let dateActuelle = '';
-  let activeLanguage = '';
-  let url = 'http://localhost:3000/api/films?page=' + pageActuelle;
-
-  if (filtrePopulariteDecroissant.get()) { 
-    filtre = filtreActif.get(); 
-    url += '&filtre=' + filtre;
-  }
-  if (filtrePopulariteCroissant.get()) { 
-    filtre = filtreActif.get(); 
-    url += '&filtre=' + filtre;
-  }
-  if (filtreGenre.get()) { 
-    genre = idGenreActif.get(); 
-    url += '&genre=' + genre;
-  }
-  if (filtreDate.get()) { 
-    dateActuelle = date.get(); 
-    url += '&date=' + dateActuelle; 
-  }
-  activeLanguage = preferedLanguageIso.get();
-  url += '&activeLanguage=' + activeLanguage;
-
-  HTTP.call(
-    'GET',
-    url,
-    {},
-    (error, response) => { 
-      movies.set(JSON.parse(response.content).results); 
-      nbPages.set(JSON.parse(response.content).total_pages);
-      nbFilms.set(JSON.parse(response.content).total_results);
-    }
-  );
-}
-
-function recupererLangagePrefere() {
-  HTTP.call(
-    'GET',
-    'http://localhost:3000/api/favLanguage',
-    {},
-    (error, response) => {
-      preferedLanguageIso.set(JSON.parse(response.content).langIso);
-      preferedLanguageName.set(JSON.parse(response.content).langName);
-      recupererTousLesFilms();
-    }
-  );
-}
-
-function recupererTousLesLangages() {
-  HTTP.call(
-    'GET',
-    'http://localhost:3000/api/languages',
-    {},
-    (error, response) => { languages.set(JSON.parse(response.content)); }
-  );
-}
-
-function recupererTousLesFilmsRecherches() {
-  HTTP.call(
-    'GET',
-    'http://localhost:3000/api/search?page=' + page.get() + '&input=' + searchInput.get(),
-    {},
-    (error, response) => { 
-      movies.set(JSON.parse(response.content).results); 
-      nbPages.set(JSON.parse(response.content).total_pages);
-      nbFilms.set(JSON.parse(response.content).total_results);
-    }
-  );
-}
-
-function recupererTousLesGenres() {
-  HTTP.call(
-    'GET',
-    'http://localhost:3000/api/genres',
-    {},
-    (error, response) => { genres.set(JSON.parse(response.content).genres); }
-  );
-}
-
-function updatePreferedLanguage(languageIso, languageName) {
-  HTTP.call(
-    'PUT',
-    'http://localhost:3000/api/languages?iso=' + languageIso + '&name=' + languageName,
-    {},
-    (error, response) => { 
-      preferedLanguageIso.set(JSON.parse(response.content).langIso); 
-      preferedLanguageName.set(JSON.parse(response.content).langName);
-    }
-  );
-}
-
-function updateLikeMovie(idMovie) {
-  HTTP.call(
-    'PUT', 
-    'http://localhost:3000/api/like/' + idMovie,
-    {},
-    (error, response) => {
-      let index = movies.get().findIndex(
-        (item) => { return item.id === JSON.parse(response.content).id; }
-      );
-      let moviesList = movies.get();
-      moviesList[index].like = JSON.parse(response.content).like;
-      movies.set(moviesList);
+        HTTP.call(
+          'GET',
+          'http://localhost:3000/low_temp',
+          {},
+          () => {
+            highTempHasPublished.set(false);
+            lowTempHasPublished.set(true);
+            stateLed.set(false);
+          }
+        );
     } 
-  );
-}
+  }
+})
--- prototypage_iot/server/main.js
+++ prototypage_iot/server/main.js
@@ -1,232 +1,85 @@
 import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
-import { HTTP } from 'meteor/http';
-import { Mongo } from 'meteor/mongo'
-import { SERVER_CONFIG } from './server-config.js';
 
-const Like = new Mongo.Collection('like');
-const Languages = new Mongo.Collection('languages');
-
-let baseurl = SERVER_CONFIG.themoviedb_api_config.base_url;
-let apikey = SERVER_CONFIG.themoviedb_api_config.api_key;
-let language = SERVER_CONFIG.themoviedb_api_config.language;
-const urlDeBase = baseurl + 'discover/movie?api_key=' + apikey + '&language=' + language;
-const urlGenres = baseurl + 'genre/movie/list?api_key=' + apikey + '&language=' + language;
-const urlSearch = baseurl + 'search/movie?api_key=' + apikey + '&language=' + language;
-const urlLanguages = baseurl + 'configuration/languages?api_key=' + apikey;
+// Ajout de la dépendance mqtt asynchrone
+var mqtt = require('async-mqtt');
 
 Meteor.startup(() => {});
 
-WebApp.connectHandlers.use('/api/favLanguage', (req, res, next) => {
-  if (!Languages.findOne()) {
-    Languages.insert({ langIso: 'fr', langName: 'French' });
-  }
-  res.writeHead(200);
-  res.end(JSON.stringify(Languages.findOne()));
-});
-
-WebApp.connectHandlers.use('/api/languages', (req, res, next) => {
-  switch (req.method) {
-    case 'GET':
-      HTTP.call(
-        'GET',
-        urlLanguages,
-        {},
-        (error, response) => {
-          let dbResp = response.data;
-          res.writeHead(200);
-          res.end(JSON.stringify(dbResp));
-        }
-      );
-      break;
-    case 'PUT':
-      let params = urlSplit(req.originalUrl);
-      let languageIso = '';
-      let languageName = '';
-      params.forEach((param) => {
-        switch (param[0]) {
-          case 'iso':
-            languageIso = param[1];
-            break;
-          case 'name':
-            languageName = param[1];
-            break;
-          default:
-            break;
-        }
-      });
-      let newPreferedLanguage = updatePreferedLanguage(languageIso, languageName);
-      res.writeHead(200);
-      res.end(JSON.stringify(newPreferedLanguage));
-      break;
-    default:
-      break;
+// Endpoint permettant d'envoyer un message sur le canal de la LED
+// sur le broker pour l'allumer
+WebApp.connectHandlers.use('/high_temp', async (req, res, next) => {
+  try {
+    // Connexion au broker
+    const client = await mqtt.connectAsync("mqtt://test.mosquitto.org");
+    // Publication sur le canal de la LED avec le payload pour l'allumage
+    await client.publish('EPSI/DHT11/adresse_mac/led', "\{ value: 1 \}");
+    res.writeHead(200);
+    res.end();
+  } catch (error) {
+    console.log(error);
+    process.exit();
   }
 });
 
-WebApp.connectHandlers.use('/api/genres', (req, res, next) => {
-  HTTP.call(
-    'GET', 
-    urlGenres,
-    {},
-    (error, response) => {
-      let genres = response.data;
-      res.writeHead(200);
-      res.end(JSON.stringify(genres));
-    }
-  );
+// Endpoint permettant d'envoyer un message sur le canal de la LED
+// sur le broker pour l'éteindre
+WebApp.connectHandlers.use('/low_temp', async (req, res, next) => {
+  try {
+    // Connexion au broker
+    const client = await mqtt.connectAsync("mqtt://test.mosquitto.org");
+    // Publication sur le canal de la LED avec le payload pour l'allumage
+    await client.publish('EPSI/DHT11/adresse_mac/led', "\{ value: 0 \}");
+    res.writeHead(200);
+    res.end();
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
 });
 
-WebApp.connectHandlers.use('/api/like', (req, res, next) => {
-  switch (req.method) {
-    case 'GET':
-      break;
-    case 'PUT':
-      let idMovie = req.url.split('/')[1];
-      let newMovieLikes = updateLikeMovie(parseInt(idMovie));
-      res.writeHead(200);
-      res.end(JSON.stringify(newMovieLikes));
-      break;
-    default:
-      break;
-    }
-  }
-);
-
-WebApp.connectHandlers.use('/api/search', (req, res, next) => {
-  let params = urlSplit(req.originalUrl);
-  let urlFinal = urlSearch;
-  let page = '';
-  let input = '';
-
-  params.forEach(param => {
-    switch (param[0]) {
-      case 'page':
-        page = param[1];
-        urlFinal += '&page=' + page;
-        break;
-      case 'input':
-        input = param[1];
-        urlFinal += '&query=' + input;
-        break;
-      default:
-        break;
-    }
-  });
-
-  HTTP.call(
-    'GET', 
-    urlFinal,
-    {},
-    (error, response) => {
-      res.writeHead(200);
-      res.end(JSON.stringify(response.data));
-    }
-  );
+// De manière asynchrone, récupération des données 
+// du canal envoyées par le capteur de température et ajout dans la
+// BDD, pour les récupérer côté client
+Meteor.publish('publish_temp', function () {
+  (async () => {
+    // Connexion au broker
+    const client = await mqtt.connectAsync("mqtt://test.mosquitto.org");
+    // Souscription au canal de la température
+    client.subscribe('EPSI/DHT11/adresse_mac/temp');
+    let hasAdded = false;
+    // Ajout dans la BDD des données
+    client.on('message', (topic, message) => {
+      if (hasAdded) {
+        this.changed('temp', '0', { celsius: JSON.parse(message.toString()) });
+      } else {
+        this.added('temp', '0', { celsius: JSON.parse(message.toString()) });
+        hasAdded = true;
+      }
+    });
+    this.ready();
+  })();
 });
 
-/**
- * Étant donné qu'à chaque nouvelle fonctionnalité de filtrage, on devait rajouter entre 
- * 2 et 5 fonctions pour gérer les filtrages multiples, nous avons décidé de tout refactoriser
- * pour avoir une fonction qui les gère tous, peu importe l'ordre/le nombre de filtres
- * 
- * D'où la fonction urlSplit(url) qui permet de récupérer les paramètres de l'url
- * */
-WebApp.connectHandlers.use('/api/films', (req, res, next) => {
-  let page = '';
-  let filtre = '';
-  let genre = '';
-  let date = '';
-  let activeLanguage = '';
-  let params = urlSplit(req.originalUrl);
-  let urlFinal = urlDeBase;
-
-  params.forEach(param => {
-    switch (param[0]) {
-      case 'page':
-        page = param[1];
-        urlFinal += '&page=' + page;
-        break;
-      case 'filtre':
-        filtre = param[1];
-        switch (filtre) {
-          case 'pod':
-            filtre = 'popularity.desc';
-            break;
-          case 'poc':
-            filtre = 'popularity.asc';
-            break;
-          default:
-            break;
-        }
-        urlFinal += '&sort_by=' + filtre;
-        break;
-      case 'genre':
-        genre = param[1];
-        urlFinal += '&with_genres=' + genre;
-        break;
-      case 'date':
-        date = param[1];
-        urlFinal += '&primary_release_year=' + date;
-        break;
-      case 'activeLanguage':
-        activeLanguage = param[1];
-        urlFinal += '&with_original_language=' + activeLanguage;
-        break;
-      default:
-        break;
-    }
-  });
-
-  HTTP.call(
-    'GET', 
-    urlFinal,
-    {},
-    (error, response) => {
-      let newResp = response.data;
-      newResp.results.forEach((movie) => {
-        let resource = Like.findOne({ id: movie.id });
-        movie.like = resource ? resource.like : 0;
-      });
-      res.writeHead(200);
-      res.end(JSON.stringify(newResp));
-    }
-  );
+// De manière asynchrone, récupération des données 
+// du canal envoyées par le capteur d'humidité et ajout dans la
+// BDD, pour les récupérer côté client
+Meteor.publish('publish_hum', function () {
+  (async () => {
+    // Connexion au broker
+    const client = await mqtt.connectAsync("mqtt://test.mosquitto.org");
+    // Souscription au canal de l'humidité
+    client.subscribe('EPSI/DHT11/adresse_mac/hum');
+    let hasAdded = false;
+    // Ajout dans la BDD des données
+    client.on('message', (topic, message) => {
+      if (hasAdded) {
+        this.changed('hum', '0', { humidity: JSON.parse(message.toString()) });
+      } else {
+        this.added('hum', '0', { humidity: JSON.parse(message.toString()) });
+        hasAdded = true;
+      }
+    });
+    this.ready();
+  })();
 });
-
-function urlSplit(url) {
-  let urlParams = url.split('?')[1].split('&');
-  let params = [];
-  urlParams.forEach(param => {
-    params.push(param.split('='));
-  });
-  return params;
-}
-
-function updateLikeMovie(idMovie) {
-  let resource = Like.findOne({ id: idMovie });
-  if (resource) {
-    Like.update(
-      { id: idMovie }, 
-      { $inc: { like: 1 } }
-    );
-  } else {
-    Like.insert(
-      { id: idMovie,
-        like: 1 }
-    );
-  }
-  return Like.findOne({ id: idMovie });
-}
-
-function updatePreferedLanguage(languageIso, languageName) {
-  let resource = Languages.findOne();
-  if (resource) {
-    Languages.update(
-      { langIso: resource.langIso },
-      { langIso: languageIso, langName: languageName });
-  } else {
-    Languages.insert({ langIso: languageIso, langName: languageName });
-  }
-  return Languages.findOne();
-}
